62f2835849605c15a5fa9d7bf2f58eca8f772a85
Matt Hartfield <razorteeth1223@gmail.com>
2026-09-07 21:50:36 -0400

    Corrected issue causing navbar to not load correctly and mess with site.

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -1,4 +1,4 @@
-fetch("/uiComponents/navbar.html")
+fetch("uiComponents/navbar.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("navbar").innerHTML = data;
